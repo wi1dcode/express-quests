152deaa60dc95917f4651a0467cb4175d9e1f2c6
unknown <magosdk@icloud.com>
2022-11-02 16:48:55 +0100

    exc done

---
+++ README.md
@@ -0,0 +1,1 @@
+"# express-quests"  git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/wi1dcode/express-quests.git git push -u origin main
---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const port = process.env.APP_PORT ?? 5000;
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+
+app.get("/", welcome);
+
+const movieHandlers = require("./movieHandlers");
+
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", movieHandlers.getUsers);
+app.get("/api/users/:id", movieHandlers.getUsersById);
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
